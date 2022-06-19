1d47d769f67cbb198b5e04eb4a5fa2ea6df5e7cd
Rushi Patel <rp970.patel@gmail.com>
2022-06-19 16:47:03 +0930

    bundle with es module

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,33 @@
 // Import path for resolving file paths
-const path = require('path');
-module.exports = {
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   entry: [path.join(__dirname, 'dist/src/index.js')],
   // Specify the output file containing our bundled code
   output: {
     path: path.join(__dirname, 'dist/bundle'),
     filename: 'index.js',
   },
+  module: {
+    rules: [{
+      test: /\.m?js/,
+      resolve: {
+        fullySpecified: false
+      }
+    }]
+  },
   target: 'node14',
   devtool: 'source-map',
   mode: 'development',
-  externals: {
-    'aws-sdk': 'require("aws-sdk")',
-  },
-  module: {},
+  externals: ['aws-sdk', {
+    ['@aws-sdk/client-dynamodb']: {
+      root: '@aws-sdk/client-dynamodb'
+    },
+    ['@aws-sdk/lib-dynamodb']: {
+      root: '@aws-sdk/lib-dynamodb'
+    }
+  }],
 };
